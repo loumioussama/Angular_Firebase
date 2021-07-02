200744dfe0cb4c0b5b0afb4a4488c631b89ddb2a
loumioussama28 <47556294+loumioussama28@users.noreply.github.com>
2021-07-02 21:51:14 +0200

    fix page signup and add firebase

--- AngularFirebase/src/environments/environment.prod.ts
+++ AngularFirebase/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAHgX-Re6FYWsVkPTRulppp67ya6E6K2pY",
+    authDomain: "angular-fire-66155.firebaseapp.com",
+    projectId: "angular-fire-66155",
+    storageBucket: "angular-fire-66155.appspot.com",
+    messagingSenderId: "132993019422",
+    appId: "1:132993019422:web:84582adda2e32eb04c880f",
+    measurementId: "G-D3D52RH281" 
+  }
 };
--- AngularFirebase/src/environments/environment.ts
+++ AngularFirebase/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAHgX-Re6FYWsVkPTRulppp67ya6E6K2pY",
+    authDomain: "angular-fire-66155.firebaseapp.com",
+    projectId: "angular-fire-66155",
+    storageBucket: "angular-fire-66155.appspot.com",
+    messagingSenderId: "132993019422",
+    appId: "1:132993019422:web:84582adda2e32eb04c880f",
+    measurementId: "G-D3D52RH281"
+  }
 };
 
 /*
